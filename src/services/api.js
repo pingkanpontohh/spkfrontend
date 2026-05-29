5a313b012bae3aa36e262446e32d681e2e5fed98
PingkanPontoh <pingkanpontoh31@gmail.com>
2026-05-29 11:24:16 +0800

    cepat

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://spkbackend-gamma.vercel.app/"
+    baseURL: "https://spkbackend-gamma.vercel.app"
 });
 
 export default API;
